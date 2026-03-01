1f66398288da72654b0721ea31309caf646538c8
Nirmal Perera <143857543+Manusha-Nirmal-Perera@users.noreply.github.com>
2026-03-01 09:46:48 +0530

    fix(baseurl): updated the base url

--- src/components/auth/GoogleLogin.jsx
+++ src/components/auth/GoogleLogin.jsx
@@ -5,7 +5,7 @@ import { LogIn } from 'lucide-react';
 export default function GoogleLogin({ text = 'Continue with Google' }) {
   const handleLogin = () => {
     const backendUrl =
-      import.meta.env.VITE_API_URL || 'http://localhost:4000/api';
+      import.meta.env.VITE_API_URL || 'http://localhost:4000/api/';
 
     window.location.href = `${backendUrl}/auth/google`;
   };
